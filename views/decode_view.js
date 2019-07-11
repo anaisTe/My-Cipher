9bf0bf8772593dfb5f1818463c7d51620d4eebb7
anaisTe <angieanais@gmail.com>
2019-07-11 17:04:56 -0500

    diseño mejorado

--- views/decode_view.js
+++ views/decode_view.js
@@ -1,30 +1,31 @@
 export const decode_Page = () => { 
 const decode_div = document.createElement("div");
 const decode = `
-<div class="" id="div_2">
-<ul class="nav justify-content-center">
-    <li class="nav-item">
-        <a class="nav-link" id="go_code">Cifrar</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" id="go_decode">Descifrar</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" id="go_out">Salir</a>
-    </li>
-</ul>
-
-<div id="container_code" class="center">
-    <p>Cifra una palabra y ten una contraseña segura</p>
-    Desplazamiento: 
+<div class="nav nav-tabs f2 navStyle" id="div_2">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link color" id="go_code">Cifrar</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" id="go_decode">Descifrar</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" id="go_out">Salir</a>
+        </li>
+    </ul>
+</div>
+<div id="container_code" class="margin center">
+    <h3 class="h3_text"><span>Decodifíca el texto secreto, es necesario tener la clave para que funcione <span></h3>
+    <span class= "back_text"> Clave: </span> 
     <input id="jump_decode" type="number" min="1" max="33" class="input-des"/>
     </br>
     <textarea id="area_decode" class="text-area" rows="4" cols="50" placeholder="Escribe aquí"></textarea>
     </br>
-    <button type="button" class="btn btn-outline-secondary" id="btn_decode">Descifrar</button>
+    <button type="button" class="btn btn-warning" id="btn_decode">Descifrar</button>
+    <button type="button" class="btn btn-danger" id="btn_reset">Reiniciar</button>
     <br/>
     <br/>
-    <p id="area2_decode"></p>
+    <p class="inst_text center f2" id="area2_decode"></p>
     <br/>
 </div>`
 
@@ -38,20 +39,26 @@ decode_div.querySelector("#go_out").addEventListener("click", ()=>{
     window.location.hash = ""
 })
 
-//
+const area_decode = decode_div.querySelector("#area_decode");
+const jump_decode = decode_div.querySelector("#jump_decode");
+const area2_decode = decode_div.querySelector("#area2_decode");
 const btn_decode = decode_div.querySelector("#btn_decode");
-    btn_decode.addEventListener("click", ()=>{
-        decode_div.querySelector("#area2_decode").value=""; 
-        const textIn_decode = decode_div.querySelector("#area_decode").value;
-        console.log(textIn_decode);
-        
-        const offsetDecode = decode_div.querySelector("#jump_decode").value;
-        console.log(offsetDecode);
-        
-        const result_decode = cipher.decode(textIn_decode , offsetDecode);
-        console.log(result_decode);
-        
-        decode_div.querySelector("#area2_decode").innerHTML=result_decode;
+const btn_reset = decode_div.querySelector("#btn_reset");
+
+//Decode the text written
+btn_decode.addEventListener("click", ()=>{
+    area2_decode.value=""; 
+    const textIn_decode = area_decode.value;        
+    const offsetDecode = jump_decode.value;    
+    const result_decode = cipher.decode(textIn_decode , offsetDecode); 
+    area2_decode.innerHTML=result_decode;
     })
+
+btn_reset.addEventListener("click", ()=>{
+    area_decode.value="";
+    jump_decode,value="";
+    area2_decode.innerHTML="";
+    })
+    
     return decode_div
 }
--- views/welcome.js
+++ views/welcome.js
@@ -2,13 +2,15 @@ export const welcome_Page = () =>{
     const welcome_div = document.createElement("div");
     const welcome = ` 
     <div class="init center" id="welcomeContainer" >
-    <h4 class="text-secondary">Welcome to</h4>
-    <h1 class="text-secondary">Irene Adler</h1>
-    <div id="container_instructions" class="show">
-    <h1 >Instructions</h1>
-    </div>
-    <button type="button" class="btn btn-outline-secondary" id="btn_start">start!</button>     
-</div> `
+        <h1 class="text-secondary">Bienvenidos a Irene Adler</h1>
+        <div id="container_instructions" class="show f2">
+            <h3 class="text-secondary">Instructiones</h3>
+            <h5 class="inst_text center f2"> Es necesario que tengas una palabra o algo en mente por esribir, 
+            luego selecciona una clave (esto indentifica la cantidad de desplazamiento para codificar) y 
+            por último selecciona codificar y listo obtendrás un texto secreto.</h5>
+        </div>
+        <button type="button" class="btn btn-outline-dark" id="btn_start">Comenzar</button>
+    </div> `
 welcome_div.innerHTML=welcome;
 
 const btn_start = welcome_div.querySelector("#btn_start");
@@ -18,3 +20,4 @@ btn_start.addEventListener("click", ()=>{
 
 return welcome_div
 }
+
